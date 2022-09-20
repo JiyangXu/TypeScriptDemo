64d1637534ec0e17542a4c501097cad03c895d27
JiyangXu <xuyang1006@hotmail.com>
2022-09-20 19:22:43 +1000

    add stage

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports={
         path:path.resolve(__dirname,'dist'),
         filename:"bundle.js",
         environment:{
-            arrowFunction:false
+            arrowFunction:false,
+            const:false
         }
     },
     mode: 'development',
