163f1853e9be8d9ce8d8ad0d945999b0a83e1f7f
JiyangXu <xuyang1006@hotmail.com>
2022-09-20 20:31:16 +1000

    add food and scorePanel class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import './style/index.less'
+import Food from "./modules/Food";
+import ScorePanel from "./modules/ScorePanel";
 
-console.log(123)
+const scorePanel = new ScorePanel();
+for(let i=0; i<200;i++){
+    scorePanel.incrementalScore();
+}
